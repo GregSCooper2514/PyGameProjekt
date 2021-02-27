b5807e3a88cedc1ad4404edb8b4ba46efe71d3c6
GregSCooper2514 <kirill.shestov123@gmail.com>
2021-02-27 19:43:15 +0300

    -

--- main.py
+++ main.py
@@ -2,15 +2,6 @@ import random, pygame, os, sys
 os.chdir("C:\\Users\\Greg\\Downloads")
 
 
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    return image
-
-
 class Cell:
     def __init__(self):
         self.is_bomb = False
